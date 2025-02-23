bd062abcd242936fcc9b2da839eec4ea1b8eeda2
Jose Alberto Delgado Robles <jadero@MacBook-Pro-de-Jose.local>
2025-02-23 12:29:41 +0100

    refactor(basic-reports): create reports lib and printer repo

--- src/repositories/basic-reports/basic-reports.module.ts
+++ src/repositories/basic-reports/basic-reports.module.ts
@@ -3,10 +3,12 @@ import { BasicReportsService } from './basic-reports.service';
 import { BasicReportsController } from './basic-reports.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Employee } from '../employees/entities/employee.entity';
+import { PrinterModule } from 'src/repositories/printer/printer.module';
+import { PrinterService } from 'src/repositories/printer/printer.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Employee])],
+  imports: [TypeOrmModule.forFeature([Employee]), PrinterModule],
   controllers: [BasicReportsController],
-  providers: [BasicReportsService],
+  providers: [BasicReportsService, PrinterService],
 })
 export class BasicReportsModule {}
